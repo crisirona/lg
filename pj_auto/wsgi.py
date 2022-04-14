98d14c6a4587ffb9d89fe8852f76dd3182617fb8
cris <cristianosvar@gmail.com>
2022-04-14 09:41:03 -0400

    conf wsgl

--- pj_auto/wsgi.py
+++ pj_auto/wsgi.py
@@ -7,10 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+project_home = '/home/crissirona'
+
+if project_home not in sys.path:
+    sys.path.insert(0,project_home)
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pj_auto.settings')
 
 application = get_wsgi_application()
+
